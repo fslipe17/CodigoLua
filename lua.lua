913deec1c18b01b18eb2a021b4856b34bda0f844
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2024-10-07 12:39:28 -0300

    Add files via upload

---
+++ lua.lua
@@ -0,0 +1,36 @@
+-- local n1 = 6
+-- local n2 = 1
+
+-- local soma = n1 + n2;
+-- local subtracao = n1 - n2
+-- divisao = n1 / n2
+-- multiplicacao = n1 * n2
+-- resto = n1 % n2
+
+
+-- print("Soma dos numeros e: " ..soma)
+-- print("Subtracao dos numeros e: " ..subtracao)
+-- print("Divisao dos numeros e: " ..divisao)
+-- print("Multiplicacao dos numeros e: " ..multiplicacao)
+-- print("Resto da divisao dos numeros e: " ..resto)
+
+-------------------------------------------------
+
+-- local n1 = 8
+-- local n2 = 2
+
+-- function Soma (n1, n2)
+--     return n1 + n2
+-- end
+
+-- print(Soma(n1, n2))
+
+-------------------------------------------------
+
+local n1 = 10
+
+if(n1 >= 10) then
+    print("O numero e maior ou igual a 10")
+else 
+    print("O numero e menor que 10")
+end
