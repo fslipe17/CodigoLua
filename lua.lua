1c999ca5cfb13372e8912fe3c034d14a2efdf7e9
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2024-10-09 16:44:04 -0300

    Add files via upload

--- lua.lua
+++ lua.lua
@@ -1,3 +1,5 @@
+---- 1. Declaração de Variáveis e Operações Aritméticas --------------------
+
 -- local n1 = 6
 -- local n2 = 1
 
@@ -14,7 +16,7 @@
 -- print("Multiplicacao dos numeros e: " ..multiplicacao)
 -- print("Resto da divisao dos numeros e: " ..resto)
 
--------------------------------------------------
+------------- 2. Função para Somar Dois Números ------------------------------------
 
 -- local n1 = 8
 -- local n2 = 2
@@ -25,12 +27,46 @@
 
 -- print(Soma(n1, n2))
 
--------------------------------------------------
+------------- 3. Condicional Simples (if/else) ------------------------------------
 
-local n1 = 10
+-- local n1 = 10
 
-if(n1 >= 10) then
-    print("O numero e maior ou igual a 10")
-else 
-    print("O numero e menor que 10")
+-- if(n1 >= 10) then
+--     print("O numero e maior ou igual a 10")
+-- else 
+--     print("O numero e menor que 10")
+-- end
+
+--------------------- 4. Criação e Manipulação de Arrays ---------------------------------------
+
+local numeros = {1,4,5,9,12}
+
+table.insert(numeros, 45)
+
+table.insert(numeros, 54)
+
+table.remove(numeros, 3)
+
+------------------- 5. Loop para Iterar sobre um Array -----------------------------------------
+
+local lista = {"java", "python", "c++"}
+
+print("Lista das limguagens de programação: ")
+
+for i = 1, #lista do
+   print("-"..lista[i])
 end
+
+-------------------- 6. Cálculo de Fatorial Usando while --------------------------------------------
+
+local num = 4
+local fatoracao = 1
+
+print("Confira o fatorial do número "..num)
+
+while num > 1 do
+    fatoracao = fatoracao * num
+    num = num - 1
+end
+
+print("Corresponde à: "..fatorial )
